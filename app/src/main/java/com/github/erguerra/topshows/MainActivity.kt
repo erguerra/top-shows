29e882d6d7c330b91fd123b475103bff11b2a0f3
erguerra <eduardo.guerra@deliverit.com.br>
2020-09-04 15:50:09 -0300

    extension of ktx at the main activity

--- app/src/main/java/com/github/erguerra/topshows/MainActivity.kt
+++ app/src/main/java/com/github/erguerra/topshows/MainActivity.kt
@@ -3,14 +3,14 @@ package com.github.erguerra.topshows
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     private val navController: NavController by lazy {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
-        navHostFragment.navController
+        findNavController(R.id.nav_host)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
